1af0804f60bea1c64f916edf3303b6705b333689
qingzz123 <ccc185416@gmail.com>
2024-12-17 16:35:10 +0800

    创建代币

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,12 +1,20 @@
 import { useRoutes, Navigate } from "react-router-dom"
 import Home from '@/views/Home'
-import About from '@/views/About'
+import CreateToken from '@/views/Token/CreateToken'
 
 
 const Routers = () => {
   const router = useRoutes([
     { path: '/', element: <Home />, },
-    { path: "about", element: <About /> },
+    {
+      path: '/token',
+      element: <Home />,
+      children: [
+        { index: true, element: <Navigate to='create' /> },
+        { path: "create", element: <CreateToken /> },
+
+      ]
+    },
   ])
 
   return router
--- src/views/Home/_defaultProps.tsx
+++ src/views/Home/_defaultProps.tsx
@@ -15,6 +15,19 @@ export default {
         name: '首页',
         icon: <BsHouseFill color={COLOR} />,
       },
+      {
+        path: '/token',
+        name: '代币管理',
+        icon: <BsHouseFill color={COLOR} />,
+        routes: [
+          {
+            path: 'create',
+            name: '创建代币',
+            icon: <BsHouseFill color={COLOR} style={{ marginRight: '6px' }} />,
+          },
+
+        ]
+      },
     ],
   },
   location: {
