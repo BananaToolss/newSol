cf8887ec0b989f1f7ae20605ae8852eaa17297bf
qingzz123 <ccc185416@gmail.com>
2024-12-18 11:14:51 +0800

    钱包

--- src/utils/wagmi.tsx
+++ src/utils/wagmi.tsx
@@ -1,66 +1,58 @@
-import { useState, useEffect, useMemo } from 'react'
+import { useState, useCallback, useMemo } from 'react'
 import {
   ConnectionProvider,
   WalletProvider,
 } from "@solana/wallet-adapter-react";
+import { WalletAdapterNetwork, WalletError } from '@solana/wallet-adapter-base';
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
 import { Connection } from "@solana/web3.js";
 import { createTheme, styled, ThemeProvider } from "@mui/material/styles";
 import { SolongWalletAdapter } from "@solana/wallet-adapter-solong";
 import { PhantomWalletAdapter } from "@solana/wallet-adapter-phantom";
-import {
-  WalletModalProvider,
-  WalletMultiButton,
-} from "@solana/wallet-adapter-react-ui";
+import { WalletModalProvider as ReactUIWalletModalProvider } from '@solana/wallet-adapter-react-ui';
 import {
   SolflareWalletAdapter,
   TorusWalletAdapter,
   LedgerWalletAdapter,
 } from '@solana/wallet-adapter-wallets';
 import "@solana/wallet-adapter-react-ui/styles.css";
-
+import { AutoConnectProvider, useAutoConnect } from './AutoConnectProvider';
+import { clusterApiUrl } from '@solana/web3.js';
 
 import { NetworkURL } from "../config";
 
 function Web3Modal({ children }) {
 
-  const [networkURL, setNetworkURL] = useState(NetworkURL);
-  const [umi, setUmi] = useState(createUmi(new Connection(networkURL)));
-  const [endpoint, setEndpoint] = useState(umi.rpc.getEndpoint());
-
-  const defaultTheme = createTheme();
-  // const wallets = [new SolongWalletAdapter(), new PhantomWalletAdapter()];
+  const { autoConnect } = useAutoConnect();
+  const network = WalletAdapterNetwork.Devnet;
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
 
   const wallets = useMemo(
     () => [
       new PhantomWalletAdapter(),
-      new SolongWalletAdapter(),
       new SolflareWalletAdapter(),
       new TorusWalletAdapter(),
-      new LedgerWalletAdapter()
+      new LedgerWalletAdapter(),
+      new SolongWalletAdapter(),
     ],
-    [networkURL]
+    [network]
   );
 
-  useEffect(() => {
-    const newUmi = createUmi(new Connection(networkURL));
-    setUmi(newUmi);
-    setEndpoint(newUmi.rpc.getEndpoint());
-  }, [networkURL]);
+  const onError = useCallback(
+    (error: WalletError) => {
+      console.error(error);
+    },
+    []
+  );
 
   return (
-    <ConnectionProvider endpoint={endpoint}>
-      <WalletProvider wallets={wallets} autoConnect>
-        <WalletModalProvider>
-          {/* <ThemeProvider theme={defaultTheme}> */}
-          {/* <SnackbarProvider maxSnack={10}> */}
-          {/* <CssBaseline /> */}
-          {children}
-          {/* </SnackbarProvider> */}
-          {/* </ThemeProvider> */}
-        </WalletModalProvider>
-      </WalletProvider>
-    </ConnectionProvider>
+    <AutoConnectProvider>
+      <ConnectionProvider endpoint={endpoint}>
+        <WalletProvider wallets={wallets} onError={onError} autoConnect={autoConnect}>
+          <ReactUIWalletModalProvider>{children}</ReactUIWalletModalProvider>
+        </WalletProvider>
+      </ConnectionProvider>
+    </AutoConnectProvider>
   )
 }
 
