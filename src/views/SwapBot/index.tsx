ccafdb79bac0cf4ac9eac1ad2206d574c7cb593a
qingzz123 <ccc185416@gmail.com>
2025-01-07 10:56:21 +0800

    1

--- src/views/SwapBot/index.tsx
+++ src/views/SwapBot/index.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { Header } from '@/components'
+import {
+  SwapBotPage,
+  LeftPage,
+  RightPage
+} from './style'
 
-function index() {
+function SwapBot() {
   return (
-    <div>index</div>
+    <SwapBotPage>
+      <Header title='市值管理' hint='预设并自动执行交易指令，轻松实现批量在DEX交易，提高了交易的效率和时效性，特别适用于快速执行大量交易的场景' />
+      <LeftPage>
+
+      </LeftPage>
+      <RightPage>
+        
+      </RightPage>
+    </SwapBotPage>
   )
 }
 
-export default index
+export default SwapBot
---
+++ src/views/SwapBot/style.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const SwapBotPage = styled.div`
+   
+`
+export const LeftPage = styled.div`
+  
+`
+
+export const RightPage = styled.div`
+  
+`
